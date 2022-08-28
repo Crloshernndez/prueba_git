1810ba5309ed2b929cbb2caf7cc5855af7e226ce
Carlos Hernandez <hernandez1989carlos@gmail.com>
2022-08-28 13:46:46 -0500

    cambio de menu
    se cambio el menu

--- main.py
+++ main.py
@@ -1,27 +1,60 @@
 # Archivo principal
+import os
 
-<<<<<<< HEAD
+
+
 print("Prueba Git")
 print("archivo descargado")
-=======
+
 print("cambio el print")
 
-def run():
-    a, b = 0, 1
-    while b <= 1597:
-        print(a, b, end=' ')
-        a = a + b
-        b = a + b
+#def run():
+#    a, b = 0, 1
+#    while b <= 1597:
+#        print(a, b, end=' ')
+#        a = a + b
+#        b = a + b
+#
+#run()
 
-run()
+
 def menu():
-    opcion = 0
-    salir = 4
-    while opcion != salir:
-        print ("menu")
-        print ("1.- ingresar alumno")
-        print ("2.- consultar alumno")
-        print ("3.- buscar alumno")
-        print ("4.- salir")
+    menu = """
+    SELECCIONE UNA OPCION
 
-        opcion = input ("ingrese una opcion: ")
+     a . ---------> ingresar alumno
+     b . ---------> consultar alumno
+     c . ---------> buscar alumno
+     d . ---------> eliminar
+
+     s . ---------> SALIR
+    """
+
+    opciones = {
+        'a' : 'ingresar alumno',
+        'b' : 'consultar alumno',
+        'c' : 'buscar alumno',
+        'd' : 'eliminar'
+    }
+
+
+    opcion_seleccionada = input(menu).lower()
+    os.system("clear")
+    while opcion_seleccionada != "s":
+        if opcion_seleccionada in opciones:
+          opcion_ejecutar = opciones[opcion_seleccionada]
+          os.system("clear")
+          input(opcion_ejecutar)
+        else :
+           os.system("clear")
+           print("Ingrese una opcion valida")
+           input("<<< Enter para continuar >>>")
+
+        opcion_seleccionada = input(menu).lower()
+
+    os.system("clear")
+
+
+menu()
+
+
