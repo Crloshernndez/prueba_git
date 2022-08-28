29f6b11ff63d689536d80e7b731788a0da3303d4
Hugo Jimenez <hgjmnz@gmail.com>
2022-08-28 13:28:59 -0500

    creacion de la funcion de menu

--- main.py
+++ main.py
@@ -14,4 +14,14 @@ def run():
         b = a + b
 
 run()
->>>>>>> 9c4758bd699586a8b30c272214c2a0ea2aec07af
+def menu():
+    opcion = 0
+    salir = 4
+    while opcion != salir:
+        print ("menu")
+        print ("1.- ingresar alumno")
+        print ("2.- consultar alumno")
+        print ("3.- buscar alumno")
+        print ("4.- salir")
+
+        opcion = input ("ingrese una opcion: ")
